c79fe1eae78a7f0ffd4746dcf5b8c041ba0f7a5f
Ally Dale <vipally@gmail.com>
2021-11-14 20:00:17 +0800

    test: add some complex test case for signature

--- go/internal/signature/query_test.go
+++ go/internal/signature/query_test.go
@@ -85,3 +85,59 @@ func TestSignature(t *testing.T) {
 	fmt.Println(Signature(x, secretKey))
 	fmt.Println(Signature(jsonBytes, secretKey))
 }
+
+func TestToQuery(t *testing.T) {
+	var testCase = map[string]interface{}{
+		"a": "f",
+		"b": []string{"foo", "bar"},
+		"c": map[string]interface{}{
+			"c1": 1,
+			"c2": map[string]interface{}{
+				"c21": "foo",
+				"c22": []interface{}{
+					[]interface{}{1, "foo"},
+					map[string]interface{}{
+						"c221": "foo",
+						"c222": true,
+					},
+				},
+			},
+		},
+		"d": []interface{}{
+			map[string]interface{}{
+				"c21": "foo",
+				"c22": []interface{}{
+					[]interface{}{1, "foo"},
+					map[string]interface{}{
+						"c221": "foo",
+						"c222": true,
+					},
+				},
+			},
+			[]interface{}{
+				[]interface{}{1, "foo"},
+				map[string]interface{}{
+					"c221": []string{"foo", "bar"},
+					"c222": true,
+				},
+			},
+		},
+	}
+
+	b, err := json.MarshalIndent(testCase, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	var d interface{}
+	if err := json.Unmarshal(b, &d); err != nil {
+		panic(err)
+	}
+
+	query := ToQuery(d)
+	expect := `a=f&b.1=foo&b.2=bar&c.c1=1&c.c2.c21=foo&c.c2.c22.1.1=1&c.c2.c22.1.2=foo&c.c2.c22.2.c221=foo&c.c2.c22.2.c222=true&d.1.c21=foo&d.1.c22.1.1=1&d.1.c22.1.2=foo&d.1.c22.2.c221=foo&d.1.c22.2.c222=true&d.2.1.1=1&d.2.1.2=foo&d.2.2.c221.1=foo&d.2.2.c221.2=bar&d.2.2.c222=true`
+	if expect != query {
+		fmt.Println(string(b))
+		t.Errorf("TestToQuery:\nexpect %s\ngot    %s\n", expect, query)
+	}
+}
